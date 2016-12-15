e6c8c405f58abe84a93eec509baf990e9def8e10
haijun <1812507678@qq.com>
2016-12-16 00:10:53 +0800

    12.16,小米风格 2016.12.5，基本界面

--- app/src/main/java/hainu/com/trainorganization/activity/BaseActivity.java
+++ app/src/main/java/hainu/com/trainorganization/activity/BaseActivity.java
@@ -3,12 +3,19 @@ package hainu.com.trainorganization.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.umeng.analytics.MobclickAgent;
 
 import hainu.com.trainorganization.R;
 
 public class BaseActivity extends Activity {
+
+    public ImageView iv_base_leftimage;
+    public TextView tv_base_rightText;
+    public TextView tv_base_centerText;
+    public ImageView iv_base_rightimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +37,65 @@ public class BaseActivity extends Activity {
 
     public void back(View view){
         finish();
+    }
+
+    public void initHeadView() {
+        iv_base_leftimage = (ImageView) findViewById(R.id.iv_base_leftimage);
+        tv_base_centerText = (TextView) findViewById(R.id.tv_base_centerText);
+        tv_base_rightText = (TextView) findViewById(R.id.tv_base_rightText);
+        iv_base_rightimage = (ImageView) findViewById(R.id.iv_base_rightimage);
+    }
+
+    public void setLeftImage(int resource) {
+        iv_base_leftimage.setImageResource(resource);
+        iv_base_leftimage.setVisibility(View.VISIBLE);
+
+    }
+
+    public void setRightImage(int resource) {
+        iv_base_rightimage.setImageResource(resource);
+        iv_base_rightimage.setVisibility(View.VISIBLE);
+    }
+
+    public void setCenterText(String text) {
+        tv_base_centerText.setText(text);
+    }
+
+    public void setRightText(String text) {
+        tv_base_rightText.setText(text);
+        tv_base_rightText.setVisibility(View.VISIBLE);
+    }
+
+
+    public ImageView getIv_base_leftimage() {
+        return iv_base_leftimage;
+    }
+
+    public void setIv_base_leftimage(ImageView iv_base_leftimage) {
+        this.iv_base_leftimage = iv_base_leftimage;
+    }
+
+    public TextView getTv_base_rightText() {
+        return tv_base_rightText;
+    }
+
+    public void setTv_base_rightText(TextView tv_base_rightText) {
+        this.tv_base_rightText = tv_base_rightText;
+    }
+
+    public TextView getTv_base_centerText() {
+        return tv_base_centerText;
+    }
+
+    public void setTv_base_centerText(TextView tv_base_centerText) {
+        this.tv_base_centerText = tv_base_centerText;
+    }
+
+    public ImageView getIv_base_rightimage() {
+        return iv_base_rightimage;
+    }
+
+    public void setIv_base_rightimage(ImageView iv_base_rightimage) {
+        this.iv_base_rightimage = iv_base_rightimage;
     }
 }
